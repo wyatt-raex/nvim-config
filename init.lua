a1dfa43e923d29eec13866068647aeca001051df
wyatt-raex <hackleywyatt@gmail.com>
2023-06-17 11:38:29 -0400

    Theme changed to projeckt0n/github-nvim-theme. Set relative numbers. LSPs for C++, Rust, & a few others installed.

--- init.lua
+++ init.lua
@@ -129,12 +129,68 @@ require('lazy').setup({
     },
   },
 
+  --[[
   {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'onedark'
+    end,
+  },
+  --]]
+
+  {
+    -- Github nvim theme
+    'projekt0n/github-nvim-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function ()
+      require('github-theme').setup({
+        options = {
+          -- Compiled file's destination location
+          compile_path = vim.fn.stdpath('cache') .. '/github-theme',
+          compile_file_suffix = '_compiled', -- Compiled file suffix
+          hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
+          hide_nc_statusline = true, -- Override the underline style for non-active statuslines
+          transparent = false,       -- Disable setting background
+          terminal_colors = true,    -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+          dim_inactive = false,      -- Non focused panes set to alternative background
+          module_default = true,     -- Default enable value for modules
+          styles = {                 -- Style to be applied to different syntax groups
+            comments = 'italic',     -- Value is any valid attr-list value `:help attr-list`
+            functions = 'NONE',
+            keywords = 'italic',
+            variables = 'NONE',
+            conditionals = 'NONE',
+            constants = 'NONE',
+            numbers = 'NONE',
+            operators = 'NONE',
+            strings = 'NONE',
+            types = 'NONE',
+          },
+          inverse = {                -- Inverse highlight for different types
+            match_paren = false,
+            visual = false,
+            search = false,
+          },
+          darken = {                 -- Darken floating windows and sidebar-like windows
+            floats = false,
+            sidebars = {
+              enable = true,
+              list = {},             -- Apply dark background to specific windows
+            },
+          },
+          modules = {                -- List of various plugins and additional options
+            -- ...
+          },
+        },
+        palettes = {},
+        specs = {},
+        groups = {},
+      })
+
+      vim.cmd('colorscheme github_dark_dimmed')
     end,
   },
 
@@ -214,6 +270,12 @@ vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Set tabwidth to 2 spaces
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandttab = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
